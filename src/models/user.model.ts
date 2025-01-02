0912b2c094f825e8351cb737890790bf11c0b7fe
bertjee321 <b.kruiter.91@gmail.com>
2025-01-02 20:11:15 +0100

    Rename model files

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,24 +1,29 @@
 export interface User {
+  id?: number;
+  email?: string;
+  password?: string; // This will be hashed
+  role?: UserRole;
+}
+
+export interface AddUser extends User {
   id: number;
   email: string;
   password: string; // This will be hashed
+  role: UserRole;
 }
 
-let users: User[] = [
-  {
-    id: 1,
-    email: "admin",
-    password: "$2a$10$ZKum3TrfzuH4u5kSM4SueesWDCYF0NkJPDSMwpXUro.3QHqqj9gay",
-  },
-]; // In-memory array to store users (replace with DB later)
-
-export function createUser(email: string, password: string): User {
-  const id = users.length + 1;
-  const newUser = { id, email, password };
-  users.push(newUser);
-  return newUser;
+export interface UpdateUser extends User {
+  id: number; // ID is required for updating a vehicle
 }
 
-export function findUserByUsername(username: string): User | undefined {
-  return users.find((user) => user.email === username);
+enum UserRole {
+  ADMIN = "admin",
+  USER = "user",
 }
+
+// export function createUser(email: string, password: string): User {
+//   const id = users.length + 1;
+//   const newUser = { id, email, password };
+//   users.push(newUser);
+//   return newUser;
+// }
